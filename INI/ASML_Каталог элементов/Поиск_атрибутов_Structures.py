1233c866eb99c5de81daa7420e89710795d96d27
alexe <alexe@MV>
2023-08-29 15:37:43 +0300

    Добавьте файлы проекта.

---
+++ INI/ASML_Каталог элементов/Поиск_атрибутов_Structures.py
@@ -0,0 +1,77 @@
+import os
+import xml.etree.ElementTree as ET
+import openpyxl
+from datetime import datetime
+
+# Путь к папке с XML-файлами
+folder_path = r"G:\BIM\WIP\03_CIVIL3D\02_Библиотеки\01_Каталоги\02_Безнапорный\ASML_Каталог элементов\Metric Structures"
+
+# Путь к файлу Excel, который нужно создать
+excel_file = r"G:\BIM\WIP\03_CIVIL3D\02_Библиотеки\01_Каталоги\02_Безнапорный\ASML_Каталог элементов\Metric Structures.xlsx"
+
+# Создание новой книги Excel
+wb = openpyxl.Workbook()
+
+# Проверка существования файла Excel и очистка данных при повторном запуске скрипта
+if os.path.exists(excel_file):
+    wb = openpyxl.load_workbook(excel_file)
+    ws = wb.active
+    
+    # Запись даты предыдущего обновления в ячейку E1
+    ws.cell(row=1, column=5).value = f"Предыдущее обновление: {ws.cell(row=2, column=5).value}"
+    
+    # Очистка данных в листе
+    for row in ws.iter_rows(min_row=2):
+        for cell in row:
+            cell.value = None
+else:
+    ws = wb.active
+    ws.title = "Metric Structures"
+
+# Заполнение заголовков столбцов
+ws.cell(row=1, column=1).value = "desc"
+ws.cell(row=1, column=2).value = "name"
+ws.cell(row=1, column=3).value = "context"
+ws.cell(row=1, column=4).value = "XML file"
+
+# Запись даты текущего обновления в ячейку E2
+ws.cell(row=2, column=5).value = f"Файл обновлен: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+
+row = 2
+
+# Список исключений для атрибута desc
+exclusions = ["Part Table", "Parameter-driven Display", "Primary Key"]
+
+# Обход всех папок и файлов в указанной директории
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        if file.endswith(".xml"):
+            file_path = os.path.join(root, file)
+            tree = ET.parse(file_path)
+            xml_root = tree.getroot()
+            
+            # Удаление расширения .xml из имени файла
+            file_name = os.path.splitext(file)[0]
+            
+            # Поиск всех элементов в XML-файле
+            for elem in xml_root.iter():
+                # Поиск атрибутов desc, name и context в элементе
+                desc = elem.get("desc")
+                name = elem.get("name")
+                context = elem.get("context")
+                
+                # Проверка наличия атрибута desc в списке исключений
+                if desc and desc in exclusions:
+                    continue
+                
+                # Запись данных в файл Excel
+                if desc or name or context:
+                    ws.cell(row=row, column=1).value = desc
+                    ws.cell(row=row, column=2).value = name
+                    ws.cell(row=row, column=3).value = context
+                    ws.cell(row=row, column=4).value = file_name
+                    
+                    row += 1
+
+# Сохранение файла Excel
+wb.save(excel_file)
---
+++ Main.cs
@@ -0,0 +1,114 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil;
+using Autodesk.Civil.ApplicationServices;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.Civil.DatabaseServices.Styles;
+using CVL3Dv23LibraryVAA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03_PPipeAlign
+{
+    public class Main
+    {
+        [CommandMethod("VAA", "PPipeAlign", CommandFlags.Modal)]
+        public static void CommandRun()
+        {
+
+            List<PressurePipeNetwork> pressurePipeNetworkList = DocumentData.CurrentCivilDocument.GetPressurePipeNetworks();
+            if (pressurePipeNetworkList.Count == 0)
+                return;
+            List<Network> networkList = DocumentData.CurrentCivilDocument.GetNetworks();
+
+
+            using (Transaction ts = DocumentData.CurrentDatabase.TransactionManager.StartTransaction())
+            {
+
+                List<PressurePipe> pressurePipeList = new List<PressurePipe>();
+                List<LineSegment3d> lineSegment3DList = new List<LineSegment3d>();
+                foreach (PressurePipeNetwork pressurePipeNetwork in pressurePipeNetworkList)
+                {
+                    //pressurePipeList.AddRange(pressurePipeNetwork.GetPipesOfPressurePipeNetwork());
+                    foreach (ObjectId pressurePipeID in pressurePipeNetwork.GetPipeIds())
+                    {
+                        PressurePipe pressurePipe = ts.GetObject(pressurePipeID, OpenMode.ForWrite, false, true) as PressurePipe;
+                        if (pressurePipe.StyleName == "ASML_ЭН_201_Кабель" || pressurePipe.StyleName == "ASML_ЭС_201_Кабель")
+                        {
+                            pressurePipeList.Add(pressurePipe);
+                        }
+                        //else
+                        //{
+                        //    MessageBox.Show("Пропyщена сеть:", pressurePipeNetwork.StyleName.ToString());
+                        //}
+                    }
+                }
+
+                List<Pipe> pipeList = new List<Pipe>(); 
+                foreach (Network network in networkList)
+                {
+                    //pipeList.AddRange(network.GetPipesOfNetwork());
+                    foreach (ObjectId pipeId in network.GetPipeIds())
+                    {
+                        Pipe pipe = ts.GetObject(pipeId, OpenMode.ForWrite, false, true) as Pipe;
+                        if (pipe.StyleName == "ASML_ЭН_201_Трубный блок" || pipe.StyleName == "ASML_ЭС_201_Трубный блок")
+                        {
+                            pipeList.Add(pipe);
+                        }
+                        //else
+                        //{
+                        //    MessageBox.Show("Пропущена сеть:", pipe.StyleName.ToString());
+                        //}
+
+                    }
+                }
+                int counter = 0;
+                foreach (Pipe pipe in pipeList)
+                {
+                    PartDataRecord pdr = pipe.PartData;
+                    double podh = (double)pdr.GetDataFieldBy("PODH").Value/1000;
+
+                    //if (lineSegment3DList.Any( ))
+                    foreach (PressurePipe pressurePipe in pressurePipeList)
+                    {
+                        LineSegment3d lineSegment3D = new LineSegment3d(PointExts.ChangeH(pressurePipe.StartPoint, 0),
+                                                                        PointExts.ChangeH(pressurePipe.EndPoint, 0));
+
+                        double pressurePipeOuterDiameter = pressurePipe.OuterDiameter;
+
+                        Tolerance normalTolerance = new Tolerance(1, 0.01);
+                        //Tolerance continuationTolerance = new Tolerance(0, 1);
+
+
+                        bool isOnlineStartPoint = lineSegment3D.IsOn(PointExts.ChangeH(pipe.StartPoint, 0), normalTolerance);
+                        bool isOnlineEndPoint = lineSegment3D.IsOn(PointExts.ChangeH(pipe.EndPoint, 0), normalTolerance);
+
+                        //bool isContinuationOnlineStartPoint = lineSegment3D.IsOn(PointExts.ChangeH(pipe.StartPoint, 0), normalTolerance);
+                        //bool isContinuationOnlineEndPoint = lineSegment3D.IsOn(PointExts.ChangeH(pipe.EndPoint, 0), normalTolerance);
+
+                        if (isOnlineStartPoint == true && isOnlineEndPoint == true)
+                        {
+                            double pipeStartPointNewZ = PointExts.GetPointZValueOnPressurePipe(pressurePipe, pipe.StartPoint) + podh / 2 - pressurePipeOuterDiameter/2;
+                            double pipeEndPointNewZ = PointExts.GetPointZValueOnPressurePipe(pressurePipe, pipe.EndPoint) + podh / 2 - pressurePipeOuterDiameter/2;
+
+                            pipe.StartPoint = new Point3d(pipe.StartPoint.X, pipe.StartPoint.Y, pipeStartPointNewZ);
+                            pipe.EndPoint = new Point3d(pipe.EndPoint.X, pipe.EndPoint.Y, pipeEndPointNewZ);
+                            counter++;
+                        }
+
+                        //else 
+                    }
+                }
+                MessageBox.Show($"Всего измененно: {counter.ToString()}","Выравнивание трубных блоков");
+
+                ts.Commit();
+            }
+            
+        }
+    }
+}
